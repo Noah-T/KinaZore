83c293e0100bde9670d5fb7ac6c73db319efea04
Noah Teshu <nteshu@gmail.com>
2014-10-29 20:38:21 -0400

    first commit

---
+++ Kina Zoré/ViewController.m
@@ -0,0 +1,68 @@
+//
+//  ViewController.m
+//  Kina Zoré
+//
+//  Created by Noah Teshu on 10/29/14.
+//  Copyright (c) 2014 Noah Teshu. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "ChimpKit.h"
+
+
+@interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *firstNameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *lastNameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *emailTextField;
+@property (nonatomic, copy) ChimpKitRequestCompletionBlock completionHandler;
+
+
+#import "ChimpKit.h"
+- (IBAction)subscribePressed:(id)sender;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    //NSDictionary *testDictionary = @{@"key1" : @"value1", @"key2" : @"value2"};
+    
+}
+
+- (IBAction)subscribePressed:(id)sender {
+//    NSDictionary *params = @{@"apikey": @"db02af20c5f964d668e994e7d1785968-us5", @"id": @"878629c097"
+//                             , @"email": @{@"email": self.emailTextField.text}, @"merge_vars": @{@"FNAME": self.firstNameTextField.text, @"LName": self.lastNameTextField.text}};
+//    NSError *error = nil;
+//    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:params options:NSJSONWritingPrettyPrinted error:&error];
+//    NSString *jsonString = [[NSString alloc]initWithData:jsonData encoding:NSUTF8StringEncoding];
+//
+//    NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"https://us5.db02af20c5f964d668e994e7d1785968-us5.mailchimp.com/2.0/lists/subscribe.json%@", jsonString]];
+//    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+//    NSURLSession *session = [NSURLSession sessionWithConfiguration:configuration];
+//
+//    NSMutableURLRequest *request = [[NSMutableURLRequest alloc]initWithURL:url];
+//    request.HTTPMethod = @"POST";
+//    
+//   
+//        NSLog(@"jsonString: %@", jsonString);
+//    
+//    NSURLSessionUploadTask *downloadTask = [session uploadTaskWithStreamedRequest:request];
+//    [downloadTask resume];
+//
+//   }
+    
+    NSDictionary *params = @{ @"id": @"878629c097"
+                             , @"email": @{@"email": self.emailTextField.text}, @"merge_vars": @{@"FNAME": self.firstNameTextField.text, @"LName": self.lastNameTextField.text, @"new-email" : self.emailTextField.text}};
+[[ChimpKit sharedKit]callApiMethod:@"lists/subscribe" withApiKey:@"db02af20c5f964d668e994e7d1785968-us5" params:params andCompletionHandler:^(NSURLResponse *response, NSData *data, NSError *error) {
+    NSLog(@"response: %@", response);
+    NSLog(@"error: %@", error);
+}];
+    
+}
+@end
+
+
+
+
---
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// ChimpKit
+#define COCOAPODS_POD_AVAILABLE_ChimpKit
+#define COCOAPODS_VERSION_MAJOR_ChimpKit 3
+#define COCOAPODS_VERSION_MINOR_ChimpKit 1
+#define COCOAPODS_VERSION_PATCH_ChimpKit 1
+
