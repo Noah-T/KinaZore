b2c91c5ba64b4d527c6e044f00363dc8faa269f2
Noah Teshu <nteshu@gmail.com>
2014-11-06 21:32:25 -0500

    added error handling to email signup

--- Kina Zoré/SubscribeViewController.m
+++ Kina Zoré/SubscribeViewController.m
@@ -15,6 +15,7 @@
 @property (weak, nonatomic) IBOutlet UITextField *lastNameTextField;
 @property (weak, nonatomic) IBOutlet UITextField *emailTextField;
 @property (weak, nonatomic) IBOutlet UITextField *zipCodeTextField;
+@property (strong, nonatomic)NSString *apiErrorKey;
 @property (nonatomic, copy) ChimpKitRequestCompletionBlock completionHandler;
 
 
@@ -33,34 +34,59 @@
 }
 
 - (IBAction)subscribePressed:(id)sender {
-//    NSDictionary *params = @{@"apikey": @"db02af20c5f964d668e994e7d1785968-us5", @"id": @"878629c097"
-//                             , @"email": @{@"email": self.emailTextField.text}, @"merge_vars": @{@"FNAME": self.firstNameTextField.text, @"LName": self.lastNameTextField.text}};
-//    NSError *error = nil;
-//    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:params options:NSJSONWritingPrettyPrinted error:&error];
-//    NSString *jsonString = [[NSString alloc]initWithData:jsonData encoding:NSUTF8StringEncoding];
-//
-//    NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"https://us5.db02af20c5f964d668e994e7d1785968-us5.mailchimp.com/2.0/lists/subscribe.json%@", jsonString]];
-//    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
-//    NSURLSession *session = [NSURLSession sessionWithConfiguration:configuration];
-//
-//    NSMutableURLRequest *request = [[NSMutableURLRequest alloc]initWithURL:url];
-//    request.HTTPMethod = @"POST";
-//    
-//   
-//        NSLog(@"jsonString: %@", jsonString);
-//    
-//    NSURLSessionUploadTask *downloadTask = [session uploadTaskWithStreamedRequest:request];
-//    [downloadTask resume];
-//
-//   }
     
-    NSDictionary *params = @{ @"id": @"878629c097"
-                              , @"email": @{@"email": self.emailTextField.text}, @"merge_vars": @{@"FIRSTNAME": self.firstNameTextField.text, @"LASTNAME": self.lastNameTextField.text, @"ZIPCODE" : self.zipCodeTextField.text, @"new-email" : self.emailTextField.text}};
-    NSLog(@"params: %@", params);
-[[ChimpKit sharedKit]callApiMethod:@"lists/subscribe" withApiKey:@"db02af20c5f964d668e994e7d1785968-us5" params:params andCompletionHandler:^(NSURLResponse *response, NSData *data, NSError *error) {
-    NSLog(@"response: %@", response);
-    NSLog(@"error: %@", error);
-}];
+    //make sure all fields are filled out
+    if (self.emailTextField.text.length == 0 || self.firstNameTextField.text.length == 0 || self.lastNameTextField.text.length == 0 || self.zipCodeTextField.text.length < 5) {
+        UIAlertView *alert = [[UIAlertView alloc]initWithTitle:@"Oops!" message:@"Please fill out all fields." delegate:nil cancelButtonTitle:@"OK" otherButtonTitles: nil];
+        [alert show];
+    } else {
+        NSDictionary *params = @{ @"id": @"878629c097"
+                                  , @"email": @{@"email": self.emailTextField.text}, @"merge_vars": @{@"FIRSTNAME": self.firstNameTextField.text, @"LASTNAME": self.lastNameTextField.text, @"ZIPCODE" : self.zipCodeTextField.text, @"new-email" : self.emailTextField.text}};
+        
+        [[ChimpKit sharedKit]callApiMethod:@"lists/subscribe" withApiKey:@"db02af20c5f964d668e994e7d1785968-us5" params:params andCompletionHandler:^(NSURLResponse *response, NSData *data, NSError *error) {
+            NSLog(@"response is: %@", response);
+            NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)response;
+            NSLog(@"the status code is: %d", httpResponse.statusCode);
+            if ([response respondsToSelector:@selector(allHeaderFields)]) {
+                NSDictionary *dictionary = [httpResponse allHeaderFields];
+                NSLog(@"%@", [dictionary description]);
+                self.apiErrorKey = [dictionary valueForKey:@"X-MailChimp-API-Error-Code"];
+            }
+            //if success, show confirmation
+            if (httpResponse.statusCode == 200) {
+                dispatch_async(dispatch_get_main_queue()
+                               , ^{
+                                   UIAlertView *alert = [[UIAlertView alloc]initWithTitle:@"Success!" message:@"Thanks for signing up!" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles: nil];
+                                   [alert show];
+                               }
+                               );
+                return;
+            }
+            //tell user if they're already signed up
+            if ([self.apiErrorKey isEqualToString:@"214"]) {
+                
+                dispatch_async(dispatch_get_main_queue()
+                               , ^{
+                                   UIAlertView *alert = [[UIAlertView alloc]initWithTitle:@"Oops!" message:@"It looks like you're already signed up." delegate:nil cancelButtonTitle:@"OK" otherButtonTitles: nil];
+                                   [alert show];
+                               }
+                               );
+                return;
+                
+            }else {
+                //general error message
+                dispatch_async(dispatch_get_main_queue()
+                               , ^{
+                                   UIAlertView *alert = [[UIAlertView alloc]initWithTitle:@"Oops!" message:@"Something went wrong. Please try again soon. If the problem continues, let us know at kinazore@gmail.com." delegate:nil cancelButtonTitle:@"OK" otherButtonTitles: nil];
+                                   [alert show];
+                               }
+                               );
+            }
+            NSLog(@"error: %@", error);
+        }];
+
+    }
+    
     
 }
 @end
